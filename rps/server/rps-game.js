a4921672bbad35c4d77a49dfbafb84995fa851d4
Juriy Bura <juriy.bura@gmail.com>
2017-10-28 23:46:56 +0200

    Lesson 4:1 - win conditions

--- rps/server/rps-game.js
+++ rps/server/rps-game.js
@@ -35,11 +35,54 @@ class RpsGame {
     const turns = this._turns;
 
     if (turns[0] && turns[1]) {
-      this._sendToPlayers('Game over' + turns.join(' : '));
+      this._sendToPlayers('Game over ' + turns.join(' : '));
+      this._getGameResult();
       this._turns = [null, null];
       this._sendToPlayers('Next Round!!!!');
     }
   }
+
+  _getGameResult() {
+
+    const p0 = this._decodeTurn(this._turns[0]);
+    const p1 = this._decodeTurn(this._turns[1]);
+
+    const distance = (p1 - p0 + 3) % 3;
+
+    switch (distance) {
+      case 0:
+        this._sendToPlayers('Draw!');
+        break;
+
+      case 1:
+        this._sendWinMessage(this._players[0], this._players[1]);
+        break;
+
+      case 2:
+        this._sendWinMessage(this._players[1], this._players[0]);
+        break;
+    }
+  }
+
+  _sendWinMessage(winner, loser) {
+    winner.emit('message', 'You won!');
+    loser.emit('message', 'You lost.');
+  }
+
+  _decodeTurn(turn) {
+    switch (turn) {
+      case 'rock':
+        return 0;
+      case 'scissors':
+        return 1;
+      case 'paper':
+        return 2;
+      default:
+        throw new Error(`Could not decode turn ${turn}`);
+    }
+  }
+
+
 }
 
 module.exports = RpsGame;
